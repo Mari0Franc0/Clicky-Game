7ba0b30f97c306bbc5e2b90376738cb497e149b1
Mario Franco <MFranc0@outlook.com>
2019-05-19 15:35:06 -0700

    fixes

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,8 +4,8 @@ import "./Banner.css";
 const Banner = () => (
   <div className="banner text-center d-flex align-items-center justify-content-center">
     <p className="m-0">
-      Test your memory. Try to click on all 12 X-men without clicking on them
-      more than once!
+      Test your memory. Try to click on all 12 X-men without clicking on the
+      same one more than once!
     </p>
   </div>
 );
